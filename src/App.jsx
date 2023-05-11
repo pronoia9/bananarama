db750d1f79b1d7c16b9dd99296da3e956d6b79a6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-12 02:40:37 +0300

    move banana to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import Bananas from './Banana';
+// Author: thesidekick (https://sketchfab.com/thesidekick)
+// License: CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+// Model: https://sketchfab.com/3d-models/banana-dda3a1f707a94c52bed79578e120937c
+
+import { Suspense, useState } from 'react';
+
+import Bananas from './components/Banana';
 
 export default function App() {
+  const [speed, setSpeed] = useState(1);
+
   return (
     <>
-      <Bananas />
+      <Suspense fallback={null}>
+        <Bananas speed={speed} />
+      </Suspense>
     </>
   );
 }
