9142ec77b36488953b0c79950ba6df246998d2ae
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-12 01:37:41 +0300

    add depth of field + fix animation + finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,37 @@ import { useRef, useState, Suspense } from 'react';
 import * as THREE from 'three';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { useGLTF, Environment } from '@react-three/drei';
+import { EffectComposer, DepthOfField } from '@react-three/postprocessing';
 
-function Banana({ z }) {
+export default function App({ count = 100, depth = 80 }) {
+  return (
+    <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 110, fov: 30 }}>
+      <color args={['#FFE47D']} attach='background' />
+
+      {/* Lights */}
+      {/* <ambientLight intensity={0.2} /> */}
+      <pointLight position={[10, 10, 10]} intensity={0.5} />
+
+      <Suspense fallback={null}>
+        {/* Objects */}
+        {Array.from({ length: count }, (_, i) => (
+          <Banana key={i} pZ={(-i / count) * depth - 20} />
+        ))}
+        {/* Effects */}
+        <EffectComposer>
+          <DepthOfField target={[0, 0, depth / 2]} focalLength={0.5} bokehScale={11} height={700} />
+        </EffectComposer>
+        <Environment preset='sunset' />
+      </Suspense>
+    </Canvas>
+  );
+}
+
+function Banana({ pZ }) {
   const { nodes, materials } = useGLTF('/banana.glb');
   const ref = useRef();
   const { viewport, camera } = useThree();
-  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, pZ]);
 
   const [data, setData] = useState({
     pX: THREE.MathUtils.randFloatSpread(2),
@@ -18,25 +43,10 @@ function Banana({ z }) {
   });
 
   useFrame((state) => {
-    ref.current.position.set(data.pX * width, (data.pY += 0.025), z);
-    if (data.pY > height / 1.5) data.pY = -height / 1.5;
-    ref.current.rotation.set((data.rX += 0.01), (data.rY += 0.004), (data.rZ += 0.005));
+    ref.current.position.set(data.pX * width, (data.pY += 0.025), pZ);
+    if (data.pY > height) data.pY = -height;
+    ref.current.rotation.set((data.rX += 0.001), (data.rY += 0.001), (data.rZ += 0.001));
   });
 
   return <mesh ref={ref} geometry={nodes.Banana.geometry} material={materials.Skin} material-emissive='orange' />;
-}
-
-export default function App({ count = 100 }) {
-  return (
-    <Canvas>
-      <ambientLight intensity={0.2} />
-      <pointLight position={[10, 10, 10]} intensity={0.5} />
-      <Suspense fallback={null}>
-        {Array.from({ length: count }, (_, i) => (
-          <Banana key={i} z={-i} />
-        ))}
-        <Environment preset='sunset' />
-      </Suspense>
-    </Canvas>
-  );
 }
