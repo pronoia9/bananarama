84447f1637b3d0188623472f00429810588bae69
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-12 02:53:15 +0300

    add fade in animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 // Model: https://sketchfab.com/3d-models/banana-dda3a1f707a94c52bed79578e120937c
 
 import { Suspense, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 import { Bananas, Overlay } from './components/';
 
@@ -13,8 +14,25 @@ export default function App() {
     <>
       <Suspense fallback={null}>
         <Bananas speed={speed} />
+        <FadeIn />
       </Suspense>
       <Overlay />
     </>
   );
 }
+
+const fade = keyframes`
+  from { opacity: 1; }
+  to { opacity: 0; }
+`;
+
+const FadeIn = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  background: #ffd863;
+  animation: ${fade} 4s normal forwards ease-in-out;
+`;
