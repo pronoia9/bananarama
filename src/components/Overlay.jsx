bc9d813c9389601386024bc4322d64e9e97060ae
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-12 03:05:44 +0300

    add styled for overlay components

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -44,15 +44,57 @@ export default function Overlay() {
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-family: 'Inter';
+  font-size: 16px;
+  & h1 {
+    padding: 0;
+    margin: 0 0 0.05em 0;
+    font-weight: 400;
+    font-size: min(18vw, 14em);
+    line-height: 0.85em;
+  }
+`;
 
-const Title = styled.div``;
+const Title = styled.h1`
+  position: absolute;
+  top: 5vw;
+  left: 5vw;
+`;
 
-const Hamburger = styled.div``;
+const Hamburger = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  top: 5vw;
+  right: 5vw;
+  & > div {
+    position: relative;
+    width: 24px;
+    height: 2px;
+    background: #252525;
+    margin-bottom: 6px;
+  }
+`;
 
-const Description = styled.div``;
+const Description = styled.div`
+  position: absolute;
+  bottom: 5vw;
+  left: 5vw;
+  width: 30ch;
+  max-width: 40%;
+`;
 
-const Poem = styled.div``;
+const Poem = styled.div`
+  position: absolute;
+  bottom: 5vw;
+  right: 5vw;
+  width: 35ch;
+  max-width: 40%;
+  line-height: 1em;
+  letter-spacing: -0.01em;
+  text-align: right;
+`;
 
 const Sketch = styled.svg`
   position: absolute;
